07060647759fff21eff14cf7571221fd4c514f55
Michael Shand <shandie231@gmail.com>
2023-11-08 17:41:37 -0500

    now able to enter coordinates and have the map update

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,62 @@
-import * as React from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import L, { LatLngTuple, Map, Rectangle } from 'leaflet';
 import { MapContainer, TileLayer } from 'react-leaflet';
 import { GeomanControl } from './Components/GeomanControl';
 import Square from './Components/Square';
 import './style.css';
+import Coordinates from './Components/Coordinates'
 
 export default function App() {
-  const washingtonDC = L.latLng(38.9072, -77.0369);
-  const farm = L.latLng(39.719303, -75.435175);
+
+  let latRef = useRef<number>();
+  let longRef = useRef<number>();
+
+  const [lat, setLat] = useState<number>(0);
+  const [long, setLong] = useState <number>(0);
+  
+  let farm = L.latLng(lat, long);
   const mapStyle = { height: '100vh', width: '100%', padding: 0 };
   const center: LatLngTuple = [51.505, -0.09]
-  const test = L.latLng(51.505, -0.09);
+  const home: LatLngTuple = [39.719303, -75.435175];
+
+
+  const updateLatLong = async (newLat: number, newLong: number): Promise<void> =>{
+    try{
+      setLat(newLat);
+      setLong(newLong);
+    }catch(err){
+      console.log('error updating lat and long');
+    }
+  }
+
 
   return (
     <React.Fragment>
       <div className='maps'>
-      <MapContainer
-        center={center}
-        zoom={15} //ranges from 0 to 18 - zero being the whole globe, 18 being fully zoomed in
-        zoomControl={false}
-        style={mapStyle}
-        whenReady={() => {}}
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <GeomanControl position="topright" drawCircle={false} oneBlock />
-        <Square center={center} size={100} />
-      </MapContainer>
+        <div id='coordinatesOverlay'>
+          <Coordinates updateLatLong={updateLatLong}/>
+        </div>
+        <h1>
+          {`lat: ${lat}, Long: ${long}`}
+        </h1>
+        <MapContainer
+          key={JSON.stringify([lat, long])}
+          center={[lat, long]}
+          zoom={15} //ranges from 0 to 18 - zero being the whole globe, 18 being fully zoomed in
+          zoomControl={false}
+          style={mapStyle}
+          whenReady={() => {}}
+        >
+
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <GeomanControl position="topright" drawCircle={false} oneBlock />
+          <Square center={home} size={100} />
+
+        </MapContainer>
+
       </div>
     </React.Fragment>
   );
--- src/Components/Coordinates.tsx
+++ src/Components/Coordinates.tsx
@@ -1,1 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+
+
+type Props = {
+  updateLatLong: any;
+
+}
+ const Coordinates = (props: Props) => {
+
+  const { updateLatLong} = props;
+  let easyLat: number;
+  let easyLong: number;
+  const [lati, setLat] = useState <number> ()  ;
+  const [longi, setLong] = useState<number>();
+
+  const changeLat = async (e: React.FormEvent<HTMLFormElement>) =>{
+    e.preventDefault();
+    updateLatLong(lati, longi);
+
+  }
+
+  return (
+    <div className='coordiantes'>
+      <h1>
+        Display Coordinates
+      </h1>
+      <form onSubmit={(e) => {changeLat(e)}}>
+        <input className="lat" type="string" placeholder="Latitude" onChange={(e) => setLat(Number(e.target.value))}></input>
+        <input className="long" type="string" placeholder="Longitude" onChange={(e) => setLong(Number(e.target.value))}></input>
+        <br></br>
+        <button className='submitLatLongBtn'>Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default Coordinates;
