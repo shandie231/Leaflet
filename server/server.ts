8caa8afb6711ec79d2607bce22a11c505171addc
Michael Shand <shandie231@gmail.com>
2023-11-09 21:48:10 -0500

    changed port, and added /favivon

--- server/server.ts
+++ server/server.ts
@@ -3,13 +3,17 @@ import path from 'path';
 
 
 const app: Express = express();
-const PORT = 3000 || process.env.PORT;
+const PORT =  process.env.PORT|| 3000;
 
 app.use(express.static(path.resolve(__dirname, '../build')));
 
+app.get('/favicon.ico', (req: Request, res: Response) => {
+  return res.status(401);
+})
 app.get('*', (req: Request, res: Response) => {
   res.sendFile(path.resolve(__dirname, '../build', 'index.html'))
 });
+
 //
 app.listen(PORT, () => {
   console.log(`listening on http://localhost:${PORT}`)
